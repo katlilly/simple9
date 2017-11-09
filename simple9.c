d37782fd42a1a888e7657d4e292cdcc6476ee946
Kat Lilly <kat.lilly@gmail.com>
2017-11-10 11:40:00 +1300

    dgap array indexing now works correctly, still have problem with not always chosing smallest possible selector

--- simple9.c
+++ simple9.c
@@ -5,11 +5,12 @@
 #include "flexarray.h"
 
 int compressedwords = 0;
+int numcompressedints = 0;
 
 struct flexarrayrec {
-    int capacity;
-    int itemcount;
-    int *items;
+    uint64_t capacity;
+    uint64_t itemcount;
+    uint64_t *items;
 };
 
 int getsize(flexarray f) {
@@ -24,10 +25,10 @@ flexarray flexarray_new() {
     return result;
 }
 
-void fappend(flexarray f, int num) {
+void fappend(flexarray f, uint64_t num) {
     if (f->itemcount == f->capacity) {
         f->capacity *= 2;
-        f->items = erealloc(f->items, f->capacity * sizeof f[0]);
+        f->items = erealloc(f->items, f->capacity * sizeof f->items[0]);
     }
     f->items[f->itemcount++] = num;
 }
@@ -35,29 +36,12 @@ void fappend(flexarray f, int num) {
 void flexarray_print(flexarray f) {
     int i;
     for (i = 0; i < f->itemcount; i++) {
-        printf("%d\n", f->items[i]);
+        printf("%llu\n", f->items[i]);
     }
 }
 
 
-void flexarray_sort(flexarray f) {
-    int p, key;
-    for (p = 1; p < f->itemcount; p++) {
-        key = f->items[p];
-        while (f->items[p-1] > key && p > 0) {
-            f->items[p] = f->items[p-1];
-            p--;
-        }
-        f->items[p] = key;
-    }
-}
-
-
-void flexarray_free(flexarray f) {
-    free(f);
-}
-
-void print_bigendian(int num) {
+void print_bigendian(uint64_t num) {
     int i;
     for (i = 31; i >= 0; i--) {
         if (num & (1<<i)) {
@@ -70,17 +54,18 @@ void print_bigendian(int num) {
 }
 
 
-unsigned int compress(unsigned int selector, int index, unsigned int *dgaps) {
-    printf("current selector: %d\n", selector);
+uint64_t compress(uint64_t selector, int thisindex, uint64_t *dgaps) {
+    printf("current selector: %llu\n", selector);
     print_bigendian(selector);
-    //selector = 3;
-    index = index - (28/selector);
-    unsigned int code, result = 0;
+    
+    //index = index - (28/selector);
+    printf("compressing from index: %d\n", thisindex);
+    uint64_t code, result = 0;
     int numcodes = 0;
     do {
         result += selector;
         printf("%dth code: \n", numcodes);
-        code = dgaps[index];
+        code = dgaps[numcompressedints];
         print_bigendian(code);
         printf("shifted code: \n");
         code = code << (4 + numcodes * selector);
@@ -88,7 +73,7 @@ unsigned int compress(unsigned int selector, int index, unsigned int *dgaps) {
         result = (result | code);
         printf("current state of compressed word: \n");
         print_bigendian(result);
-        index++;
+        numcompressedints++;
         numcodes++;
     } while (((numcodes+1) * selector) < 29);
     //printf("compressing with selector %c\n", selector);
@@ -100,18 +85,20 @@ unsigned int compress(unsigned int selector, int index, unsigned int *dgaps) {
 int compare_ints(const void *a, const void *b) {
     const int *ia = (const int *) a;
     const int *ib = (const int *) b;
-    return (*ia > *ib) - (*ia < *ib);
+    //return (*ia > *ib) - (*ia < *ib);
+    return *ia < *ib ? -1 : *ia == *ib ? 0 : 1;
 }
 
 
 int main(void) {
-    unsigned int selector;
-    unsigned int arraysize = 500, i, prev = 0;
-    unsigned int *docnums, *dgaps;
-    unsigned int bits, maxbits, current, elements;
-    unsigned int index = 0;
+    uint64_t selector;
+    uint64_t arraysize = 500, i, prev = 0;
+    uint64_t *docnums, *dgaps;
+    uint64_t bits, maxbits, current, elements;
+    uint64_t index = 0;
+    int columns;
     
-    flexarray compressed = flexarray_new();
+    flexarray compresseddgaps = flexarray_new();
     
     //flexarray f = flexarray_new();
     //
@@ -125,16 +112,12 @@ int main(void) {
     docnums = emalloc(arraysize * sizeof docnums[0]);
     for (i = 0; i < arraysize; i++) {
         docnums[i] = rand() % (arraysize *3);
-        printf("%d, ", docnums[i]);
+        //printf("%llu, ", docnums[i]);
     }
     
     qsort(docnums, arraysize, sizeof docnums[0], compare_ints);
-    
+  
     dgaps = emalloc(arraysize * sizeof dgaps[0]);
-    
-    //for (i = 0; i < arraysize; i++) {
-    //    dgaps[i] = 0;
-    //}
     
     for (i = 0; i < arraysize; i++) {
         dgaps[i] = docnums[i] - prev;
@@ -142,23 +125,34 @@ int main(void) {
     }
     
     printf("d-gaps:\n");
+    
+    columns = 0;
     for (i = 0; i < arraysize; i++) {
-        printf("%d\n", dgaps[i]);
+        printf("%4llu ", dgaps[i]);
+        columns++;
+        if(columns == 10) {
+            printf("\n");
+            columns = 0;
+        }
     }
     
     while (index < arraysize) {
         elements = 0;
         maxbits = 0;
+        index = numcompressedints;
 
-        do {
-            current = dgaps[index];
-            index++;
+        // this doesn't work right, in some cases we compress with
+        // more bits than needed because the following element needs it.
+        // does a valid encoding, but with more sometimes bits than necessary
+        while ((maxbits * elements) < 29){
+            current = dgaps[index++];
             elements++;
             bits = fls(current);
+            printf("%llu bits, ", bits);
             if (bits > maxbits) {
                 maxbits = bits;
             }
-        } while (maxbits * elements < 29);
+        } //while (maxbits * elements < 29);
         
         
         if (maxbits > 28) {
@@ -174,21 +168,22 @@ int main(void) {
         } else {
             selector = maxbits;
         }
-        
-        int temp = compress(selector, index, dgaps);
-        fappend(compressed, temp);
+        printf("compressing %dth word with selector %llu\n", compressedwords, selector);
+        printf("current index is: %llu\n", index);
+        int temp = compress(selector, numcompressedints, dgaps);
+        fappend(compresseddgaps, temp);
         compressedwords++;
     }
     
     //compress(3, 35, dgaps);
-    flexarray_print(compressed);
-    printf("number of integers compressed: %d\n", arraysize);
+    flexarray_print(compresseddgaps);
+    printf("number of integers compressed: %llu\n", arraysize);
     printf("compressed into %d words\n", compressedwords);
     
     printf("first compressed word in binary: \n");
-    print_bigendian(compressed->items[0]);
+    print_bigendian(compresseddgaps->items[0]);
     
-    flexarray_free(compressed);
+    free(compresseddgaps);
     free(dgaps);
     free(docnums);
     
