62c82ff32d76b4050cb3ab8a9af1ddba9e564d4f
Kat Lilly <kat.lilly@gmail.com>
2017-11-16 13:30:10 +1300

    ready to start tests with limited selectors

--- findbestselectors.c
+++ findbestselectors.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<strings.h>
 #include<stdint.h>
+#include<math.h>
 #include "flexarray.h"
 
 // make synthetic dgap data
@@ -44,70 +45,142 @@ int compare_ints(const void *a, const void *b) {
 
 
 int main(void) {
-  int i, prev = 0;
-  int arraysize = 1000;
-  int gap = 0, maxgap = 0;
-  int *docnums, *dgaps;
+    int i, bits, sum, prev = 0;
+    int freqarraysize, arraysize = 1000;
+    int gap = 0, maxgap = 0;
+    int *docnums, *dgaps;
 
-  // make set of random numbers to use as document numbers
-  docnums = malloc(arraysize * sizeof docnums[0]);
-  for (i = 0; i < arraysize; i++) {
-    docnums[i] = rand() % (arraysize * 500);
-  }
-
-  // sort them and convert to dgaps
-  qsort(docnums, arraysize, sizeof docnums[0], compare_ints);
-  dgaps = malloc(arraysize * sizeof dgaps[0]);
-  for (i = 0; i < arraysize; i++) {
-    dgaps[i] = docnums[i] - prev;
-    prev = docnums[i];
-  }
-
-  // find dgap frequencies
-  for (i = 0; i < arraysize; i++) {
-    gap = dgaps[i];
-    if (gap > maxgap) {
-      maxgap = gap;
+    // make set of random numbers to use as document numbers
+    docnums = malloc(arraysize * sizeof docnums[0]);
+    for (i = 0; i < arraysize; i++) {
+        docnums[i] = (rand() % (arraysize * 50000)/100);
     }
-  }
-  printf("maxgap: %d\n", maxgap);
-  
-  bitlengthfreqs = malloc(20 * sizeof bitlengthfreqs[0]);
-  for (i = 0; i < 20; i++) {
-    bitlengthfreqs[i] = 0;
-  }
-  
-  for (i = 0; i < arraysize; i++) {
-    if (dgaps[i] < 2) {
-      bitlengthfreqs[1]++;
-    } else if (dgaps[1] < 4) {
-      bitlegnthfreqs[2]++;
-    } else if (dgaps[i] < 8) {
-      bitlengthfreqs[3]++;
-    } else if (dgaps[1] < 16) {
-      bitlegnthfreqs[4]++;
-    } else if (dgaps[i] < 32) {
-      bitlengthfreqs[5]++;
-    } else if (dgaps[1] < 64) {
-      bitlegnthfreqs[6]++;
-    } else if (dgaps[i] < 2) {
-      bitlengthfreqs[7]++;
-    } else if (dgaps[1] < 4) {
-      bitlegnthfreqs[9]++;
-    } else if (dgaps[i] < 8) {
-      bitlengthfreqs[10]++;
-    } else if (dgaps[1] < 16) {
-      bitlegnthfreqs[11]++;
-    } else if (dgaps[i] < 32) {
-      bitlengthfreqs[]++;
-    } else if (dgaps[12] < 64) {
-      bitlegnthfreqs[6]++;
-    } else if (dgaps[13] < 2) {
-      bitlengthfreqs[7]++;
-    } else if (dgaps[14] < 4) {
-      bitlegnthfreqs[8]++;
-    } else if 
 
-  return 0;
+    // sort them and convert to dgaps
+    qsort(docnums, arraysize, sizeof docnums[0], compare_ints);
+    dgaps = malloc(arraysize * sizeof dgaps[0]);
+    for (i = 0; i < arraysize; i++) {
+        dgaps[i] = docnums[i] - prev;
+        prev = docnums[i];
+    }
+
+    // find size of max dgap before making array of bitlengths
+    for (i = 0; i < arraysize; i++) {
+        gap = dgaps[i];
+        if (gap > maxgap) {
+            maxgap = gap;
+        }
+    }
+    printf("maxgap: %d\n", maxgap);
+    freqarraysize = fls(maxgap) + 1;
+  
+    // array to hold bit length frequencies
+    int *bitlengthfreqs = malloc(freqarraysize * sizeof bitlengthfreqs[0]);
+    for (i = 0; i < freqarraysize; i++) {
+        bitlengthfreqs[i] = 0;
+    }
+
+    /* // set frequencies 256 bit version */
+    /* for (i = 0; i < arraysize; i++) { */
+    /*     bits = fls(dgaps[i]); */
+    /*     if (bits > 32) { */
+    /*         return(EXIT_FAILURE); */
+    /*     } else if (bits > 21) { */
+    /*         bits = 32; */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } else if (bits > 16) { */
+    /*         bits = 21; */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } else if (bits > 12) { */
+    /*         bits = 16; */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } else if (bits > 10) { */
+    /*         bits = 12; */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } else { */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } */
+    /* } */
+    /* printf("14 valid bit lengths:\n"); */
+    /* for (i = 0; i < freqarraysize; i++) { */
+    /*     printf("number of dgaps requiring %d bits: %d\n", i, bitlengthfreqs[i]); */
+    /* } */
+    /* printf("\n"); */
+    /* int sum = 0; */
+    /* for (i = 0; i < freqarraysize; i++) { */
+    /*     sum += bitlengthfreqs[i]; */
+    /* } */
+    /* printf("sum (should be 1000): %d\n", sum); */
+  
+    
+    /* // set frequencies 128 bit version */
+    /* for (i = 0; i < freqarraysize; i++) { */
+    /*     bitlengthfreqs[i] = 0; */
+    /* } */
+    /* for (i = 0; i < arraysize; i++) { */
+    /*     bits = fls(dgaps[i]); */
+    /*     if (bits > 32) { */
+    /*         return(EXIT_FAILURE); */
+    /*     } else if (bits > 16) { */
+    /*         bits = 32; */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } else if (bits > 10) { */
+    /*         bits = 16; */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } else if (bits > 8) { */
+    /*         bits = 10; */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } else if (bits > 6) { */
+    /*         bits = 8; */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } else { */
+    /*         bitlengthfreqs[bits]++; */
+    /*     } */
+    /* } */
+    /* printf("10 valid bit lengths:\n"); */
+    /* for (i = 0; i < freqarraysize; i++) { */
+    /*     printf("number of dgaps requiring %d bits: %d\n", i, bitlengthfreqs[i]); */
+    /* } */
+    /* printf("\n"); */
+    /* sum = 0; */
+    /* for (i = 0; i < freqarraysize; i++) { */
+    /*     sum += bitlengthfreqs[i]; */
+    /* } */
+    /* printf("sum (should be 1000): %d\n", sum); */
+
+
+    //set frequencies
+    for (i = 0; i < freqarraysize; i++) {
+        bitlengthfreqs[i] = 0;
+    }
+    for (i = 0; i < arraysize; i++) {
+        bits = fls(dgaps[i]);
+        if (bits < 2) { //won't be zeros in real data, ones should be run length encoded
+            bits = 1;
+            bitlengthfreqs[bits]++;
+        } else if (bits < 17) {
+            bitlengthfreqs[bits]++;
+        } else if (bits < 32) {
+            bits = 16;
+            bitlengthfreqs[bits]++;
+        } else if (bits == 32) {
+            bitlengthfreqs[bits]++;
+        } else {
+            return(EXIT_FAILURE);
+        }
+    }
+  
+
+    printf("unrestricted bit lengths:\n");
+    for (i = 0; i < freqarraysize; i++) {
+        printf("number of dgaps requiring %d bits: %d\n", i, bitlengthfreqs[i]);
+    }
+    sum = 0;
+    for (i = 0; i < freqarraysize; i++) {
+        sum += bitlengthfreqs[i];
+    }
+    printf("sum (should be 1000): %d\n", sum);
+    
+    return 0;
 
 }
