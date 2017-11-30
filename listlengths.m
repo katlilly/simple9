23a12d052a796a19b0f88744e15a6d095e0a6ff3
Kat Lilly <kat.lilly@gmail.com>
2017-12-01 10:15:55 +1300

    added matlab script and graph for total items in lists of given lengths

--- listlengths.m
+++ listlengths.m
@@ -5,5 +5,15 @@ cumulative = 41205930
 %error = cumulative - check
 
 plot(lengths, totalitems, 'bo')
-title(Total items in lists of a given size range)
-ylable()
+title('Total items in lists of a given size range')
+axis([0 6 0 18e6])
+
+xticks([1 2 3 4 5])
+xticklabels({'<100','<1000','<10000','<100000','100000+'})
+
+%## cumulative length of all lists: 41205930
+%## 3076857
+%## 7461238
+%## 17443866
+%## 12949474
+%## 274495
