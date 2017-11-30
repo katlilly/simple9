12343b6384d7ce4ba1fc984e6a85110649835aaa
Kat Lilly <kat.lilly@gmail.com>
2017-11-30 13:52:54 +1300

    matlab files for plotting selector use vs bitwidth statistics for two individual lists

---
+++ plotlist445139.m
@@ -0,0 +1,60 @@
+list445139 = csvread('list445139.csv');
+
+listnumber = list445139(1,1)
+length = list445139(2,1)
+
+rawbitwidths = list445139(4:34,:);
+
+
+
+% row 44 is start of 1 bit list
+% rown 286 is end of 28 bit list
+
+
+%**************************************
+%*** plot where n-bit ints went *******
+%**************************************
+bitsbyselector = list445139(44:286,:);
+bitwidth1 = bitsbyselector(1:9,2)
+bitwidth2 = bitsbyselector(10:18,2)
+bitwidth3 = bitsbyselector(19:27,2)
+bitwidth4 = bitsbyselector(28:36,2)
+
+selectors = (1:9)
+
+plot(selectors, bitwidth1, selectors, bitwidth2, selectors, bitwidth3, selectors, bitwidth4)
+xlabel('selector')
+ylabel('number of ints')
+title('List #445139, 157870 postings')
+legend('1-bit ints','2-bit ints','3-bit ints','4-bit ints')
+
+
+
+axis([1 9 0 100000])
+
+xticks([1 2 3 4 5 7 9 14])
+  
+%*************************************
+%**** plot how selectors were used ***
+%*************************************
+selectorbybits = list445139(287:end,:);
+
+selector1 = selectorbybits(2:28,2);
+selector2 = selectorbybits(30:56,2);
+selector3 = selectorbybits(58:84,2);
+selector4 = selectorbybits(86:112,2);
+selector5 = selectorbybits(114:140,2);
+selector7 = selectorbybits(142:168,2);
+selector9 = selectorbybits(170:196,2);
+selector14 = selectorbybits(198:224,2);
+selector28 = selectorbybits(226:252,2);
+fls = (1:27);
+
+%plot(fls, selector1, fls, selector2, fls, selector3, ...
+%     fls, selector4, fls, selector5)
+%xlabel('bit width')
+%ylabel('number of ints')
+%title('List #445139, 157870 postings')
+%  legend({'1 bit selector', '2 bit selector','3 bit selector','4 bit selector','5 bit selector'})
+
+%axis([1 5 0 80000])
