c4a0935665ccdfff969c4733701c76202720c19f
Kat Lilly <kat.lilly@gmail.com>
2018-01-11 10:33:42 +1300

    calculated ways to pack k ints into n bits

--- waystopack.c
+++ waystopack.c
@@ -1,57 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <strings.h>
 
-struct twoints {
-    int a;
-    int b;
-};
 
-struct twoints twointways[20];
-
-int payloadbits = 32;
-
-int getcombinations(int intstopack) {
-    int numcombinations = 0;
-
-    if (intstopack < 1 || intstopack > 32) {
-        printf("invalid number of ints to pack\n");
-        return EXIT_FAILURE;
-    }
-    if (intstopack == 1) {
-        return 
-    }
-
-    
-    // this logic only works for packing two ints
-    for (int i = 1; i < payloadbits / intstopack; i++) {
-        printf("bits for int a: %d, bits for int b: %d\n", i, 32-i);
-        numcombinations++;
-    }
-    printf("number of ways to pack %d ints: %d\n", intstopack, numcombinations);
-    return numcombinations;
+long long choose(n, k)
+{
+    if (k == 0) return 1;
+    return (n * choose(n-1, k-1)) / k;
 }
+    
+
+
 
 int main(void)
-    {
-
-    int i, combinations, permutations;
-
+{
+    int i;
+    long long ways; /* every permutation */
+    int payloadbits = 32;
+    
     for (i = 1; i <= payloadbits; i++) {
-        printf("number of ways to pack %d ints:\n", i);
-        combinations = getcombinations(i);
-        
-        
-    }
-
-    for (i = 0; i < 20; i++) {
-        twointways[i].a = i+1;
-        twointways[i].b = payloadbits - twointways[i].a;
-        combinations++;
+        ways = choose(payloadbits - 1, i - 1);
+        printf("ways to pack %d ints in %d bits: ", i, payloadbits);
+        printf("%lld \t(%d bits)\n", ways, fls(ways));
     }
     
-    printf("number of combinations: %d\n", combinations);
-
-    
-
     return 0;
 }
